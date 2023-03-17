750d02c08cafa06054fc2aa4dfef189f5fa33a17
Данила Хренов <danilahreno@gmail.com>
2023-03-17 13:13:26 +0300

    Добавьте файлы проекта.

---
+++ SPP/First.xaml.cs
@@ -0,0 +1,38 @@
+﻿using SPP.fppDataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SPP
+{
+    /// <summary>
+    /// Логика взаимодействия для First.xaml
+    /// </summary>
+    public partial class First : Page
+    {
+        usersTableAdapter users = new usersTableAdapter();
+        public First()
+        {
+            InitializeComponent();
+            UsersGrid.ItemsSource = users.GetData();
+        }
+
+        private void ChangeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var data = users.GetData();
+            users.InsertQuery(data[data.Count - 1].id + 1, NewText.Text, $"{NewText.Text}@mail.com");
+            UsersGrid.ItemsSource = users.GetData();
+        }
+    }
+}
---
+++ SPP/SP.xaml.cs
@@ -0,0 +1,53 @@
+﻿using SPP.fppDataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SPP
+{
+    /// <summary>
+    /// Логика взаимодействия для SP.xaml
+    /// </summary>
+    public partial class SP : Page
+    {
+        chatsTableAdapter chats = new chatsTableAdapter();
+        usersTableAdapter users = new usersTableAdapter();
+        public SP()
+        {
+            InitializeComponent();
+            ChatsGrid.ItemsSource = chats.GetData();
+            FU.ItemsSource = users.GetData();
+            FU.DisplayMemberPath = "id";
+            SU.ItemsSource = users.GetData();
+            SU.DisplayMemberPath = "id";
+        }
+
+        private void ChangeBtn1_Click(object sender, RoutedEventArgs e)
+        {
+            if (FU.SelectedIndex != -1 && SU.SelectedIndex != -1)
+            {
+                var idNew = chats.GetData();
+                int fUser = users.GetData()[FU.SelectedIndex].id;
+
+                int sUser = users.GetData()[SU.SelectedIndex].id;
+                Debug.WriteLine(SU.SelectedIndex);
+                chats.InsertQuery(idNew[idNew.Count - 1].id + 1, fUser, sUser);
+                ChatsGrid.ItemsSource = chats.GetData();
+            }
+
+        }
+    }
+}
